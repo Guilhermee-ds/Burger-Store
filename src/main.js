9813f427d1cae6d7318fb94dc7f4efe0e90a0c33
Guilhermesantosoliv <contato.guilhermedossantos@gmail.com>
2023-09-23 17:56:45 -0300

    darkmode function completed

--- src/main.js
+++ src/main.js
@@ -62,16 +62,34 @@ tabs.forEach(tab =>{
     })
 })
 
-const scrollUp = () => {
-    const scrollUpBtn = document.getElementById("scroll-up")
 
-    if(this.scrollY >= 250){
-        scrollUpBtn.classList.remove("-bottom-1/2");
-        scrollUpBtn.classList.add("bottom-4");
-    } else {
-        scrollUpBtn.classList.add("-bottom-1/2");
-        scrollUpBtn.classList.remove("bottom-4");
-    }
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
 }
 
 window.addEventListener('scroll', scrollUp)
